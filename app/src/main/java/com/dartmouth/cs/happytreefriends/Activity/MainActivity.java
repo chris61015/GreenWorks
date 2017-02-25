7b6ddb5cd328211eb123075e8158475491fdf0d0
盧元駿 <chris61015@gmail.com>
2017-02-25 18:51:26 -0500

    Build Up Project Structure

--- app/src/main/java/com/dartmouth/cs/happytreefriends/Activity/MainActivity.java
+++ app/src/main/java/com/dartmouth/cs/happytreefriends/Activity/MainActivity.java
@@ -1,8 +1,9 @@
 package com.dartmouth.cs.happytreefriends.Activity;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+import com.dartmouth.cs.happytreefriends.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,7 +18,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
